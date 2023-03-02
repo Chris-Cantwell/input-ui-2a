2ebe875aa47f156bd520b79beb626e6c28bb12d4
Chris Catwell <christophercantwell@college.harvard.edu>
2023-03-02 15:31:57 -0500

    Begin table formatting

---
+++ src/routes/calendar/+page.svelte
@@ -0,0 +1,29 @@
+<!-- This is our / route -->
+<script lang=ts>
+	import Grid from "./Grid.svelte";
+	export let days: number[] = [0, 1, 0, 0, 0, 1, 0]
+	export let days2: number[] = [0, 1, 0, 2, 0, 1, 0]
+</script>
+
+<svelte:head>
+	<title>Calendar Testing</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		New Route for Testing
+	</h1>
+
+</section>
+
+<Grid days={days}/>
+
+<br>
+<br>
+
+<Grid days={days2}/>
+
+<style>
+
+</style>
---
+++ src/routes/calendar/Grid.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let days = [0,0,0,0,0,0,0]
+    export let hours = [days, days, days]
+</script>
+
+<center>
+    <table>
+        <tr>
+            <th>Sunday</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+        </tr>
+        {#each hours as hour}
+            <tr>
+            {#each days as day}
+                <td>{day}</td>
+            {/each}
+            </tr>
+        {/each}
+    </table>
+</center>
